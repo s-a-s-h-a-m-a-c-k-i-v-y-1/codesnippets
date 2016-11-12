15de04c0dfa79d4a4dc85cfe21f24721ce4626ad
s-a-s-h-a-m-a-c-k-i-v-y-1 <jukumakikorohati@gmail.com>
2016-11-12 12:41:05 +0530

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -255,3 +255,73 @@ void Sieve()
             arrVal[i] = arrVal[i-1];
     }
 }
+//Prime between two numbers
+int main() {
+    vector<int> primes;
+    primes.push_back(2);
+    
+    for (int i = 3; i <= 32000; i+=2) {
+        bool isprime = true;
+        int cap = sqrt(i) + 1;
+        
+        vector<int>::iterator p;
+        for (p = primes.begin(); p != primes.end(); p++) {
+            if (*p >= cap) break;
+            if (i % *p == 0) {
+                isprime = false;
+                break;
+            }
+        }
+        if (isprime) primes.push_back(i);
+    }
+    
+    int T,N,M;
+    
+    cin >> T;
+    
+    for (int t = 0; t < T; t++) {
+        if (t) cout << endl;
+        
+        cin >> M >> N;
+        if (M < 2) M = 2;
+        
+        int cap = sqrt(N) + 1;
+        
+        set<int> notprime;
+        notprime.clear();
+        
+        vector<int>::iterator p;
+        for (p = primes.begin(); p != primes.end(); p++) {
+            
+            if (*p >= cap) break;
+            int start;
+            
+            if (*p >= M) start = (*p)*2;
+            else start = M + ((*p - M % *p) % *p);
+            
+            for (int j = start; j <= N; j += *p) {
+                notprime.insert(j);
+            }
+        }
+        
+        for (int i = M; i <= N; i++) {
+            if (notprime.count(i) == 0) {
+                cout << i << endl;
+            }
+        }
+        
+    }
+    return 0;
+}
+///////Lagrange INterpolation
+/*for(ll i = 0; i < s; i ++) cin >> a[i];
+for(ll i = s; i < s + c; i ++){
+    double val = 0;
+    for(ll j = 0; j < s; j ++){
+        double p = 1;
+        for(ll k = 0; k < s; k ++) if(j != k) p *= ((double)i - (double)k) / ((double)j - (double)k);
+            val += ((double)a[j] * p);
+            }
+    cout << roundl(val) << " ";
+    }
+*/
